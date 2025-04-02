582fb04d52c5ae82150d81e4d51a2386ab8a576a
matteo borgio <matteo6.borgio@gmail.com>
2025-04-02 09:15:31 +0200

    aggiunta e modifica di print e miglioramento della logica nella funzione main

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def mostra_tabellone(tabellone: list[list[str]]) -> None:
     """Stampa la griglia di gioco in modo leggibile."""
     for riga in tabellone:
         print(" ".join(riga))
+    print("")
 
 def gioca_turno(tabellone: list[list[str]], giocatore: str) -> None:
     """Gestisce l'input del giocatore e aggiorna il tabellone."""
@@ -29,13 +30,14 @@ def gioca_turno(tabellone: list[list[str]], giocatore: str) -> None:
         while True:
             try:
                 colonna = int(input("Inserisci la colonna in cui vuoi inserire il simbolo: "))
+                print("")
                 if colonna >= len(tabellone[0]) or colonna < 0:
                     raise ValueError
                 break
             except:
                 print("Inserisci un numero valido!")
         if tabellone[riga][colonna] != '_':
-            print("Questa casella è già occupata!")
+            print("Questa casella è già occupata!\n")
         else:
             tabellone[riga][colonna] = giocatore
             return None
@@ -54,17 +56,24 @@ def verifica_vittoria(tabellone: list[list[str]]) -> None | str:
 
 def aggiorna_punteggio(giocatori: dict, segno_vincente: str) -> None:
     """Aggiorna il punteggio del giocatore vincente e stampa il vincitore."""
-
     for key in ["player1", "player2"]:
         if giocatori[key]["simbolo"] == segno_vincente:
             giocatori[key]["vittorie"] += 1
             print("")
-            print(f"Ha vinto {giocatori[key]['nome']}!")
+            print(f"Ha vinto {giocatori[key]['nome']}!\n")
+            print(f"{giocatori[key]['nome']} ora ha {giocatori[key]['vittorie']}!")
+            print("")
             return None
-        
-def partita(giocatori: dict, tabellone: list[list[str]]) -> None:
+
+def partita(giocatori: dict, tabellone: list[list[str]], simboli: list[str]) -> None:
     """Gestisce il flusso principale del gioco, alternando i turni e determinando il risultato finale."""
     segno_vincente = None
+    shuffle(simboli)
+    giocatori["player1"]["simbolo"] = simboli[0]
+    giocatori["player2"]["simbolo"] = simboli[1]
+    print(f"Il giocatore {giocatori['player1']['nome']} ha il simbolo {giocatori['player1']['simbolo']}\n")
+    print(f"Il giocatore {giocatori['player2']['nome']} ha il simbolo {giocatori['player2']['simbolo']}\n")
+    mostra_tabellone(tabellone)
     while True:
         gioca_turno(tabellone, giocatori["player1"]["simbolo"])
         mostra_tabellone(tabellone)
@@ -72,7 +81,6 @@ def partita(giocatori: dict, tabellone: list[list[str]]) -> None:
         if segno_vincente is not None:
             break  
         if all('_' not in riga for riga in tabellone): 
-            print("") 
             print("Pareggio!")  
             return  
         gioca_turno(tabellone, giocatori["player2"]["simbolo"])
@@ -81,44 +89,43 @@ def partita(giocatori: dict, tabellone: list[list[str]]) -> None:
         if segno_vincente is not None:
             break  
         if all('_' not in riga for riga in tabellone):  
-            print("")
             print("Pareggio!")  
             return 
-
     aggiorna_punteggio(giocatori, segno_vincente)
 
 def main() -> None:
     """Gestisce la sfida al meglio dei tre e dichiara il vincitore finale."""
+    print("")
     print("Benvenuti in tic tac toe! \n")
-    for i in range(3):
-        tabellone = inizializza_tabellone()
-        simboli = ["O", "X"]
-        shuffle(simboli)
-        name_player1 = input("Inserisci il nome del primo giocatore: ")
-        name_player2 = input("Inserisci il nome del secondo giocatore: ")
-        print("")
-        giocatori = {
-            "player1": {
-                "nome": name_player1,
-                "simbolo": simboli[0],
-                "vittorie": 0
-                },
-            "player2": {
-                "nome": name_player2,
-                "simbolo": simboli[1],
-                "vittorie": 0
-            }
+    simboli = ["O", "X"]
+    shuffle(simboli)
+    name_player1 = input("Inserisci il nome del primo giocatore: ")
+    name_player2 = input("Inserisci il nome del secondo giocatore: ")
+    print("")
+    giocatori = {
+        "player1": {
+            "nome": name_player1,
+            "simbolo": simboli[0],
+            "vittorie": 0
+            },
+        "player2": {
+            "nome": name_player2,
+            "simbolo": simboli[1],
+            "vittorie": 0
         }
-        partita(giocatori, tabellone)
+    }
+    for _ in range(3):
+        tabellone = inizializza_tabellone()
+        if giocatori["player1"]["vittorie"] == 2 or giocatori["player2"]["vittorie"] == 2:
+            break
+        partita(giocatori, tabellone, simboli)
     if giocatori["player1"]["vittorie"] > giocatori["player2"]["vittorie"]:
-        print("")
         print(f"Il vincitore è {giocatori['player1']['nome']} con {giocatori['player1']['vittorie']} vittorie!")
     elif giocatori["player1"]["vittorie"] < giocatori["player2"]["vittorie"]:
-        print("")
         print(f"Il vincitore è {giocatori['player2']['nome']} con {giocatori['player2']['vittorie']} vittorie!")
     else:
-        print("")
         print("Pareggio tra i due giocatori!")
 
 if __name__ == "__main__":
     main()
+
